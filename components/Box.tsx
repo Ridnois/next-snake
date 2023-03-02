ecf040aa8cdcbd8db7d69a06f5825219749862bb
Sebastian Altamirano <ridnois@protonmail.com>
2023-03-02 01:27:30 -0300

    box sizes for desktop

--- components/Box.tsx
+++ components/Box.tsx
@@ -7,8 +7,8 @@ export function Box(props: BoxProps) {
   const snakebg = rand === 0 ? 'bg-cyan-500' : 'bg-cyan-500 opacity-90'
   return (
     <div className={`
-         h-[22px] md:h-[44px]
-         w-5 md:w-10
+         h-[22px] md:h-[42px]
+         w-5 md:w-[42px]
          rounded-md
          ${props.state === 'snake' ? snakebg : props.state === 'food' ? 'bg-red-500 shadow-2xl shadow-slate-100' : 'dark:bg-slate-900 bg-slate-200'}
         `}
